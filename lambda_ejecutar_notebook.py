68fce5fa2737642f82699e9d186ae97356382b3e
SantiDzA <santiago06diaz@gmail.com>
2024-12-16 00:44:27 +0100

    arreglar indentación

--- lambda_ejecutar_notebook.py
+++ lambda_ejecutar_notebook.py
@@ -3,4 +3,17 @@ import forecast_aws
 
 def lambda_handler(event, context):
     forecast_aws.main()
+    invocar_siguiente()
     return {"statusCode": 200, "body": "Predicción realizada correctamente"}
+
+def invocar_siguiente():
+    print("Entrando al invocador")
+    client = boto3.client('lambda')
+    try:
+        client.invoke(
+            FunctionName="s3_a_athena",
+            InvocationType='Event'
+        )
+        print(f"Lambda 's3_a_athena' invocada exitosamente.")
+    except Exception as e:
+        print(f"Error al invocar Lambda 's3_a_athena': {str(e)}")
