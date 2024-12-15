0fa945db15729b3fc609405ca577a2cb1fb14339
SantiDzA <santiago06diaz@gmail.com>
2024-12-15 20:12:37 +0100

    actualizaciones varias

--- forecast_aws.py
+++ forecast_aws.py
@@ -1,15 +1,16 @@
-
-print("Importando...")
+import boto3
+import zipfile
+import sys
+import pandas as pd
+from io import StringIO
+#Importar statsmodels
 s3_client = boto3.client('s3')
-s3_client.download_file("almacenamiento-primario", 'layers_lambda/statsmodels_layer.zip', '/tmp/statsmodels_layer.zip')
-print("Descargado con éxito")
-with zipfile.ZipFile('/tmp/statsmodels_layer.zip', 'r') as zip_ref:
+s3_client.download_file("almacenamiento-primario", 'layers_lambda/statsmodels_package.zip', '/tmp/statsmodels_package.zip')
+with zipfile.ZipFile('/tmp/statsmodels_package.zip', 'r') as zip_ref:
     zip_ref.extractall('/tmp/')
 sys.path.append('/tmp/python')
-print("Extraído con éxito")
-import statsmodels
-#from pmdarima import auto_arima
-print("Importado con éxito")
+#import statsmodels.api as sm
+from statsmodels.tsa.arima.model import ARIMA
 
 def main():
     # Conexiones con DynamoDB
@@ -23,88 +24,18 @@ def main():
 
     # Arreglo de datos
     df_dynamo["fecha"] = pd.to_datetime(df_dynamo["fecha"])
-
-    df_dynamo = df_dynamo.sort_values(by="fecha", ascending=True).reset_index(drop=True)
-    df_dynamo = df_dynamo[["fecha", "CLOSE_GSPC", "CLOSE_IBEX", "CLOSE_N225"]]
-
-    # Ajustando el index
     df_dynamo.set_index("fecha", inplace=True)
+    df_dynamo = df_dynamo.sort_values(by="fecha", ascending=True)
+    print(df_dynamo.head())
 
-    # Ajustando la frecuencia
-    df_dynamo = df_dynamo.asfreq("b")
-
-    # Valores faltantes
-    df_dynamo["CLOSE_IBEX"] = df_dynamo["CLOSE_IBEX"].fillna(method="ffill")
-    df_dynamo["CLOSE_GSPC"] = df_dynamo["CLOSE_GSPC"].fillna(method="ffill")
-    df_dynamo["CLOSE_N225"] = df_dynamo["CLOSE_N225"].fillna(method="ffill")
-
-
-    # Modelo
-    modelo_ibex = auto_arima(df_dynamo["CLOSE_IBEX"])
-    modelo_sp500 = auto_arima(df_dynamo["CLOSE_GSPC"])
-    modelo_nikkei = auto_arima(df_dynamo["CLOSE_N225"])
-
-
-    # Predicciones
-    forecast_stocks = pd.concat(
-        [
-            pd.DataFrame(
-                {
-                    "fecha": modelo_ibex.predict(
-                        n_periods=5, return_conf_int=True, alpha=0.05
-                    )[0].index,
-                    "forectast_ibex_inf": modelo_ibex.predict(
-                        n_periods=5, return_conf_int=True, alpha=0.05
-                    )[1][:, 0],
-                    "forecast_ibex": modelo_ibex.predict(
-                        n_periods=5, return_conf_int=True, alpha=0.05
-                    )[0].values,
-                    "forecast_ibex_sup": modelo_ibex.predict(
-                        n_periods=5, return_conf_int=True, alpha=0.05
-                    )[1][:, 1],
-                }
-            ),
-            pd.DataFrame(
-                {
-                    "forectast_nikkei_inf": modelo_nikkei.predict(
-                        n_periods=5, return_conf_int=True, alpha=0.05
-                    )[1][:, 0],
-                    "forecast_nikkei": modelo_nikkei.predict(
-                        n_periods=5, return_conf_int=True, alpha=0.05
-                    )[0].values,
-                    "forecast_nikkei_sup": modelo_nikkei.predict(
-                        n_periods=5, return_conf_int=True, alpha=0.05
-                    )[1][:, 1],
-                }
-            ),
-            pd.DataFrame(
-                {
-                    "forectast_nikkei_inf": modelo_nikkei.predict(
-                        n_periods=5, return_conf_int=True, alpha=0.05
-                    )[1][:, 0],
-                    "forecast_nikkei": modelo_nikkei.predict(
-                        n_periods=5, return_conf_int=True, alpha=0.05
-                    )[0].values,
-                    "forecast_nikkei_sup": modelo_nikkei.predict(
-                        n_periods=5, return_conf_int=True, alpha=0.05
-                    )[1][:, 1],
-                }
-            ),
-        ],
-        axis=1,
-    )
-
-    df_dynamo.reset_index(inplace=True)
-
-    # Configurar cliente S3
-    s3 = boto3.client("s3")
+    #FIXME
 
     # Convertir el DataFrame a CSV
     csv_buffer = StringIO()
-    forecast_stocks.to_csv(csv_buffer, index=False)
+    df_dynamo.to_csv(csv_buffer, index=False)
 
     # Subir a S3
-    bucket_name = "exit-bucket-sae"  # Reemplaza con el nombre de tu bucket
-    s3_key = "forecast_stocks.csv"  # Ruta donde se guardará el archivo en S3
-    s3.put_object(Bucket=bucket_name, Key=s3_key, Body=csv_buffer.getvalue())
+    bucket_name = "almacenamiento-primario"
+    s3_key = "forecast_stocks.csv"
+    s3_client.put_object(Bucket=bucket_name, Key=s3_key, Body=csv_buffer.getvalue())
     print(f"Archivo subido a S3: s3://{bucket_name}/{s3_key}")
--- image/src/lambda_ejecutar_notebook.py
+++
@@ -1,6 +0,0 @@
-import forecast_aws
-
-
-def lambda_handler(event, context):
-    forecast_aws.main()
-    return {"statusCode": 200, "body": "Predicción realizada correctamente"}
